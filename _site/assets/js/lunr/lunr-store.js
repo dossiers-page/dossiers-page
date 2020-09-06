76142d974e6cbefba41b19aadc01c6e2de173b46
Gireesan Namboothiri P <sahyagiri@gmail.com>
2020-09-06 14:52:31 +0530

    added/updated tags

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Richard Feynman",
         "excerpt":"If you have not heard of him, he is a Drummer who can play Bongos Artist Someone who could listen to the lowest level of “rocket science” and tell NASA that they should often believe their own engineers and move the launch dates than the top management calling the shots...","categories": [],
         "tags": ["movies","biopic","books","inspiration","physics"],
-        "url": "http://localhost:4000/richard-feynman/",
+        "url": "https://sahyagiri.github.io/richard-feynman/",
         "teaser": null
       }]
