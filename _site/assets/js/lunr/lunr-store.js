aa2034a26acb7d1e4eddb4b0d4a88a7843990dcb
Gireesan Namboothiri P <sahyagiri@gmail.com>
2020-09-07 06:47:56 +0530

    updated the feynman post with addl book links

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Richard Feynman",
-        "excerpt":"If you have not heard of him, he is A drummer who can play Bongos An artist ![Feynman’s drawing] (https://lh5.ggpht.com/__zoKJ77EvEc/SzjdjKxgguI/AAAAAAAACQU/XkOX5pj6XNU/feynman-art%20%2817%29%5B2%5D.jpg?imgmax=1200) Someone who could listen to the lowest level of “rocket science” and tell NASA that they should often believe their own engineers and move the launch dates rather than the...","categories": [],
+        "excerpt":"If you have not heard of him, he is A drummer who can play Bongos An artist Someone who could listen to the lowest level of “rocket science” and tell NASA that they should often believe their own engineers and move the launch dates rather than the top management calling...","categories": [],
         "tags": ["movies","biopic","books","inspiration","physics"],
         "url": "http://localhost:4000/richard-feynman/",
         "teaser": null
