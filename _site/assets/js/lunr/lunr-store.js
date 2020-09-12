bcb6790c09248cdd6d06533cdc3272670c877181
Gireesan Namboothiri P <sahyagiri@gmail.com>
2020-09-12 22:09:00 +0530

    added new post on heart disease

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["movies","biopic","books","inspiration","physics"],
         "url": "http://localhost:4000/richard-feynman/",
         "teaser": null
+      },{
+        "title": "How to reduce heart disease risk? An exploratory Model Analysis on open data set",
+        "excerpt":"Age going above 35 is when some of us starts thinking about fitness (and obesity). While there are many points of inspiration, mine are purely on reducing heart disease risk, particularly when it comes as hereditary. There are many questions I asked myself. Major ones are How do I know...","categories": [],
+        "tags": ["inspiration","EMA","exploratory_model_analysis","machine_learning"],
+        "url": "http://localhost:4000/how-to-reduce-heart-disease-risk-an-exploratory-model-analysis-on-open-data-set/",
+        "teaser": null
       }]
